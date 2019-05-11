b2d8f076715743ad3595833032392318dcccf839
Esteban Ramirez <estebanramirezcc@gmail.com>
2019-05-11 12:08:38 -0500

    Initial Commit

---
+++ movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movies
+//
+//  Created by Esteban on 5/11/19.
+//  Copyright © 2019 Esteban Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
